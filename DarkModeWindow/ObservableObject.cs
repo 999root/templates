8f73e09486bbf2fb2785ec0c3e4b82f31c353cab
echo 999root <WhiteAirForcesWithMud@protonmail.com>
2023-07-23 18:04:38 +0100

    Create ObservableObject.cs

---
+++ DarkModeWindow/ObservableObject.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FilePatcherGUI
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
